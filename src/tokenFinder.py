3ff473c35088cf5c241f5ca710a060d65b6dd9d9
TanujKS <tanujsiripurapu@gmail.com>
2021-11-01 14:30:02 -0700

    Fixed tokenFinder.py

--- src/tokenFinder.py
+++ src/tokenFinder.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-from sys import platform
+from sys import platform as _platform
 
 
 class Token:
@@ -16,7 +16,7 @@ class Token:
 
 class TokenFinder:
     def __init__(self):
-        if platform  not in ["darwin", "linux"]:
+        if _platform  not in ["darwin", "linux"]:
             input("WARNING: This program is only supported for macOS \nThere is no gaurantee on the functionality of this program on this device. \nPress enter to continue")
 
         tokens = []
